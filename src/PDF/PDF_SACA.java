b810114065f5204e90db0b04f68e8fc93bc49297
pl05 <pl05@PL05.fin.com>
2018-03-19 08:56:55 +0100

    poprawka sqla

--- src/PDF/PDF_SACA.java
+++ src/PDF/PDF_SACA.java
@@ -427,11 +427,11 @@ public class PDF_SACA {
 				addHeaderW(wazneSaca);
 				float widthsWazne[] = new float[] {10, 10, 12, 14, 8, 8, 8, 10, 5};
 				String sqlwazne2 = "Select projekt, NrZamowienia2, kodartykulu, nazwaartykulu, sum(pozostalodoprojektu) as pozostalodoprojektu, pozostalonazamowieniu, jednostka, cel, typ from " + 
-						" (Select projekt, NrZamowienia2, kodartykulu, nazwaartykulu, pozostalodoprojektu, pozostalonazamowieniu, jednostka, cel, saca.typ from saca "
+						" (Select projekt, NrZamowienia2, kodartykulu, nazwaartykulu, pozostalodoprojektu, pozostalonazamowieniu, jednostka, cel, saca.typ, calendar.dataprodukcji from saca "
 						+ "join calendar on saca.projekt = calendar.nrMaszyny "
 						+ "where datadodania = '"+dzis+"' and wazne = 1 "
 								+ "group by NrZamowienia2, kodartykulu "
-								+ "order by calendar.dataProdukcji asc, projekt asc, saca.typ desc, saca.kodartykulu) T group by KodArtykulu";
+								+ "order by calendar.dataProdukcji asc, projekt asc, saca.typ desc, saca.kodartykulu) T group by KodArtykulu order by T.dataprodukcji";
 				Statement Parts = myConn.createStatement();
 				ResultSet rsParts = Parts.executeQuery(sqlwazne2);
 				while(rsParts.next()) {
